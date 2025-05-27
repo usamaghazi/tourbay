42f7ee3df41b4418e8f85bdc66c031267042a9fa
Muhammad Usama <mohammad.usama441@gmail.com>
2025-05-27 16:05:57 +0500

    Arrange Folder Structure

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native'
+import { Container } from '../Styles/RootPage'
+const RootPage = () => {
+  return (
+    <Container>
+      <Text>RootPage</Text>
+    </Container>
+  )
+}
+
+export default RootPage
