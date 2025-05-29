d7fe30d5a6c70e160477a3dd82ba2af2d02f51f7
Muhammad Usama <mohammad.usama441@gmail.com>
2025-05-29 13:02:30 +0500

    Card Style Completed

--- components/UserCard/Styles.js
+++ components/UserCard/Styles.js
@@ -1,8 +1,9 @@
+import { Ionicons } from '@expo/vector-icons';
 import styled from 'styled-components/native';
 import Colors from '../../constants/Colors';
 
 export const Card = styled.View({
-    height:'55%',
+    height:'57%',
     width:'90%',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
@@ -10,11 +11,44 @@ export const Card = styled.View({
     shadowRadius: 4,
     elevation: "3px",
     backgroundColor:Colors.background,
-    paddingTop: 20,
+    paddingTop: 25,
     paddingBottom:20,
     margin: 5,
     borderRadius:5,
-    alignItems:'center',
-    // justifyContent:'center'
-    
+    // alignItems:'center',
+})
+
+export const ImageWrapper = styled.View({
+    height: '33%',
+    width:'33%',
+    borderRadius:50,
+    backgroundColor:Colors.inputBorder,
+    alignSelf:'center'
+})
+
+export const UserName = styled.Text({
+    marginTop:20,
+    fontSize:21,
+    fontWeight:'bold',
+    color: Colors.textTitle,
+    alignSelf:'center'
+})
+
+export const UserInfoWrapper = styled.View({
+    flexDirection:'row',
+    marginTop:20,
+    paddingLeft:30,
+    paddingRight:30
+})
+
+export const Icon = styled(Ionicons)({
+    marginBottom:10
+})
+
+export const UserInfoText = styled.Text({
+    fontSize:15,
+    color:Colors.textInfo,
+    fontWeight:'bold',
+    marginBottom:8,
+    marginLeft:7
 })
--- components/UserCard/index.jsx
+++ components/UserCard/index.jsx
@@ -1,11 +1,46 @@
-import { Text } from 'react-native'
-import { Card } from './Styles'
+
+import { View } from 'react-native'
+import Colors from '../../constants/Colors'
+import { Card, Icon, ImageWrapper, UserInfoText, UserInfoWrapper, UserName } from './Styles'
+
 const UserCard = () => {
+
+  const iconName = ['person-outline','mail-outline','call-outline','location-outline','globe-outline']
+
   return (
     <Card>
-        <Text>
-            Good
-        </Text>
+        <ImageWrapper/>
+        <UserName>
+          goldencat511
+        </UserName>
+        <UserInfoWrapper>
+            <View>
+            {iconName.map(ionicon=>(
+              <Icon
+              key={ionicon}
+              name={ionicon}
+              size={18}
+              color={Colors.iconColor}/>
+            ))}
+          </View>
+          <View>
+            <UserInfoText>
+              Evenly Geri
+            </UserInfoText>
+            <UserInfoText>
+              evenly.wang@gmail.com
+            </UserInfoText>
+            <UserInfoText>
+               (186)-188-3421
+            </UserInfoText>
+            <UserInfoText>
+              Upper Hutt
+            </UserInfoText>
+            <UserInfoText>
+              New Zealand
+            </UserInfoText>
+          </View>
+        </UserInfoWrapper>
     </Card>
   )
 }
