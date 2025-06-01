725e6ab520fdc4708f84410de4a1bd3fb9c33ced
Muhammad Usama <mohammad.usama441@gmail.com>
2025-06-01 12:11:19 +0500

    Searching

--- hooks/useFetchUsers.js
+++ hooks/useFetchUsers.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 const useFetchUsers = () => {
   const [nameEmail, setNameEmail] = useState('')
+  const [filterUsers, setFilterUsers] = useState([]) //new Work
   const [allUsers, setAllUsers] = useState([]) // Store ALL 50 users
   const [displayedUsers, setDisplayedUsers] = useState([]) // Store 10 Users
   const [loading, setLoading] = useState(false)
@@ -23,6 +24,7 @@ const useFetchUsers = () => {
   
             const userData = await responce.json()
             setAllUsers(userData.results)
+            setFilterUsers(userData.results)
             setDisplayedUsers(userData.results.slice(0, USERS_PER_BATCH))
   
         } catch (error) {
@@ -36,11 +38,39 @@ const useFetchUsers = () => {
       handleFetchUsers()
     },[])
 
+    useEffect(()=>{
+      if(nameEmail.trim()===''){
+        setFilterUsers(allUsers)
+        setDisplayedUsers(filterUsers.slice(0,USERS_PER_BATCH))
+      }
+      else {
+        const searchLetters = nameEmail.toLocaleLowerCase().trim()
+        const filtered = allUsers.filter(user => {
+          const userName = user.login?.username?.toLowerCase() || ''
+          const firstName = user.name?.first?.toLowerCase() || ''
+          const lastName = user.name?.last?.toLowerCase() || ''
+          const email = user.email?.toLowerCase() || ''
+          const fullName = `${firstName} ${lastName}`.toLowerCase()
+
+          return(
+            userName.startsWith(searchLetters) ||
+            firstName.startsWith(searchLetters) ||
+            lastName.startsWith(searchLetters) ||
+            email.startsWith(searchLetters) ||
+            fullName.startsWith(searchLetters) 
+          )
+        })
+
+        setFilterUsers(filtered)
+        setDisplayedUsers(filtered.slice(0,USERS_PER_BATCH))
+      }
+    },[nameEmail,allUsers]) //new Work
+
     const loadMoreUsers = () => {
     if(loadingMore) return 
 
     const currentUsers = displayedUsers.length
-    const remainingUsers = allUsers.length - currentUsers
+    const remainingUsers = filterUsers.length - currentUsers
 
     if(remainingUsers <= 0) return
 
@@ -48,7 +78,7 @@ const useFetchUsers = () => {
 
     setTimeout(()=>{
       const nextUsers = currentUsers + USERS_PER_BATCH
-      const newBatch = allUsers.slice(currentUsers,nextUsers)
+      const newBatch = filterUsers.slice(currentUsers,nextUsers)
       if (newBatch.length > 0) {
         setDisplayedUsers(pre => [...pre, ...newBatch])
       }
